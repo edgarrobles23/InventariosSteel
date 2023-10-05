4daae40d781228e3b52c05d2b220e2793a2d2b81
acaballerozapata <adan.caballero.zapata.84@gmail.com>
2023-10-05 12:28:16 -0600

    .

--- ApiSteel-NET6/Program.cs
+++ ApiSteel-NET6/Program.cs
@@ -90,21 +90,21 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+//app.UseStaticFiles();
 app.UseRouting();
 app.UseCors(corsPolicy);
-
+app.MapGet("/", () => "Hello World!");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html"); 
+//app.MapFallbackToFile("index.html"); 
 
 app.Run();
